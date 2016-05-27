3372258f1f263941bd2208ef18bbd12f4b799315
GuntisKlav <guntisklavins17@inbox.lv>
2016-05-28 01:19:09 +0300

    komentari

--- Funkcijas/funkcijas.php
+++ Funkcijas/funkcijas.php
@@ -97,17 +97,17 @@ function viensIeraksts(){
 		
 	$get_ieraksti = "SELECT * FROM ieraksti WHERE ieraksta_id='$get_id'";
 	
-	$run_ieraksti = mysqli_query($con, $get_posts);
+	$run_ieraksti = mysqli_query($con, $get_ieraksti);
 	
 	$rindas_ieraksti=mysqli_fetch_array($run_ieraksti);
 	
-		$ieraksta_id = $rindas_ieraksti['post_id'];
-		$lietotaja_id = $rindas_ieraksti['user_id'];
-		$saturs = $rindas_ieraksti['post_content'];
-		$ieraksta_datums = $rindas_ieraksti['post_date'];
+		$ieraksta_id = $rindas_ieraksti['ieraksta_id'];
+		$lietotaja_id = $rindas_ieraksti['lietotaja_id'];
+		$saturs = $rindas_ieraksti['ieraksta_saturs'];
+		$ieraksta_datums = $rindas_ieraksti['ieraksta_datums'];
 		
 		//Iegūšt lietotāju kurš publicēja ierakstu
-		$lietotajs = "SELECT * FROM lietotaji WHERE lietotaja_id='$lietotaja_id' AND posts='yes'"; 
+		$lietotajs = "SELECT * FROM lietotaji WHERE id='$lietotaja_id' AND posts='yes'"; 
 		
 		$run_lietotajs = mysqli_query($con, $lietotajs); 
 		$row_lietotajs=mysqli_fetch_array($run_lietotajs);
@@ -128,7 +128,7 @@ function viensIeraksts(){
 		
 		<p><img src='Lietotājs/Lietotaja_bildes/$lietotaja_bilde' width='50' height='50'></p>
 		<h3><a href='user_profile.php?id=$lietotaja_id'>$lietotajvards</a></h3> 
-		<p>$eraksta_datums</p>
+		<p>$ieraksta_datums</p>
 		<p>$saturs</p>
 		
 		</div>
@@ -136,21 +136,21 @@ function viensIeraksts(){
 		include("comments.php");
 		
 		echo "
-		<form action='' method='post' id='reply'>
-		<textarea cols='50' rows='5' name='comment' placeholder='Ieraksti savu komentāru'></textarea><br/>
-		<input type='submit' name='reply' value='Komentēt'/>
+		<form action='' method='post' id='atbilde'>
+		<textarea cols='50' rows='5' name='komentars' placeholder='Ieraksti savu komentāru'></textarea><br/>
+		<input type='submit' name='atbilde' value='Komentēt'/>
 		</form>
 		";
 		
-		if(isset($_POST['reply'])){
+		if(isset($_POST['atbile'])){
 		
-			$comment = $_POST['comment'];
+			$komentars = $_POST['komentars'];
 			
-			$insert = "insert into comments (post_id,user_id,comment,comment_author,date) values ('$post_id','$user_id','$comment','$user_com_name',NOW())";
+			$insert = "INSERT INTO komentari (ieraksta_id,lietotaja_id, komentars, komentara_autors, date) VALUES ('$ieraksta_id','$lietotaja_id','$komentars', '$lietotajvards', NOW())";
 			
 			$run = mysqli_query($con,$insert); 
 			
-			echo "<h2>Your Reply was added!</h2>";
+			echo "<h2>Tavs komentārs tika pievienots.</h2>";
 			
 		
 		}
