708120787483734972f737139223c412f7f18b17
GuntisKlav <guntisklavins17@inbox.lv>
2016-05-29 03:50:29 +0300

    Header dizains

--- Template/header.php
+++ Template/header.php
@@ -3,6 +3,7 @@
 <html> 
 	<head>
 		<title>Mountain Maniacs</title>
+
 	<link rel="stylesheet" href="styles/style.css" media="all"/>
 	</head>
 	
@@ -11,8 +12,11 @@
 	<div class="container">
 		<!--Head wrap starts-->
 		<div id="head_wrap">
+		<h1>MOUNTAIN MANIACS</h1>
+		<button id="admin"><a href="Administrators/admin_login.php">Administrators</a></button>
 			<!--Header starts-->
 			<div id="header">
+			
 				<form method="post" action="" id="form1">
 					<input type="email" name="email1" placeholder=" Epasts" required="required" /> 
 					<input type="password" name="pass1" placeholder=" Parole" required="required"/> 
